7b2b80b788fd98450bc4d17bbe496bce1a970927
Risshab Singla <risshab2singla@gmail.com>
2024-01-16 15:47:38 +0530

    Map width responsive

--- frontend/src/pages/ContactUs.js
+++ frontend/src/pages/ContactUs.js
@@ -1,4 +1,7 @@
+import useScreenSize from "../utils/useScreenSize";
+
 function ContactUs() {
+  const screen = useScreenSize();
   return (
     <div>
       <div className="p-3 text-3xl text-blue-900 font-extrabold text-center">
@@ -43,7 +46,7 @@ function ContactUs() {
           </div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.040998284477!2d79.15335867506292!3d12.969228414930324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bad479f0ccbe067%3A0xfef222e5f36ecdeb!2sVellore%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1705324748852!5m2!1sen!2sin"
-            width="600"
+            width={Math.min(600, screen.width - 20)}
             height="400"
             style={{ border: "0" }}
             allowfullscreen=""
