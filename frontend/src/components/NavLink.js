ef82ef60922979f83f5485066e3acf9f5352b8c0
Risshab Singla <risshab2singla@gmail.com>
2024-01-16 15:27:28 +0530

    Width adjustment

--- frontend/src/components/NavLink.js
+++ frontend/src/components/NavLink.js
@@ -1,9 +1,7 @@
 function NavLink({ href, children }) {
   return (
     <li className="text-white bg-blue-500 rounded-lg m-2 p-2">
-      <a className="w-screen" href={href}>
-        {children}
-      </a>
+      <a href={href}>{children}</a>
     </li>
   );
 }
