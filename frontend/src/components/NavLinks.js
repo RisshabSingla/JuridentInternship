8a22e8509230d6071143e4d28ab82eab4843b7af
Risshab Singla <risshab2singla@gmail.com>
2024-01-16 12:35:52 +0530

    Changed Position

--- frontend/src/components/NavLinks.js
+++ frontend/src/components/NavLinks.js
@@ -4,12 +4,12 @@ function NavLinks() {
     <>
       <NavLink href={"/"}> Home</NavLink>
       <NavLink href={"aboutus"}> About Us</NavLink>
+      <NavLink href={"ourmission"}> Our Mission</NavLink>
       <NavLink href={"blogs"}> Blogs</NavLink>
+      <NavLink href={"projects"}> Projects</NavLink>
+      <NavLink href={"events"}> Events</NavLink>
       <NavLink href={"contactus"}> Contact Us</NavLink>
       <NavLink href={"getinvolved"}> Get Involved</NavLink>
-      <NavLink href={"projects"}> Projects</NavLink>
-      <NavLink href={"ourmission"}> Our Mission</NavLink>
-      <NavLink href={"events"}> Events</NavLink>
     </>
   );
 }
